f489455015ac43010e33d0654560317bc70c209e
odan <odan3240@gmail.com>
2016-01-04 19:43:12 +0900

    AI関係ファイルのリファクタリング

--- src/MidGameAI.hpp
+++ src/MidGameAI.hpp
@@ -2,90 +2,22 @@
 #define INCLUDE_CROSSWALK_MIDGAME_AI_HPP
 
 #include "Board.hpp"
+#include "AI.hpp"
 #include "Utils.hpp"
 
 namespace crosswalk {
 
 template<class Eval>
-class MidGameAI {
-
-    Eval eval;
-    i64 score;
-public:
-    CellType operator()(const Board &board, CellState color, i32 depth) noexcept {
-        CellType result(-1,-1);
-        i64 alpha = minValue<i64>();
-        i64 beta = maxValue<i64>();
-        i64 a=alpha;
-        i64 b=beta;
-        i64 evalValue  = minValue<i64>();
-        i64 maxValue = evalValue;
-        bool first=true;
-
-        auto cells = board.makeReversibleCells(color);
+class MoveOrdering {
+    void operator()(Cells &cells, const Board &board, CellState color) noexcept {
+        Eval eval;
         moveOrdering(cells, board, color, 8, eval, std::greater<i64>());
-
-        for(const auto &cell : cells) {
-            auto nextBoard = board;
-            nextBoard.putStone(color, cell);
-
-            evalValue = -dfs(nextBoard, switchCellState(color), -b, -a, depth - 1);
-            if(a<evalValue && evalValue<beta && !first) a=-dfs(nextBoard, switchCellState(color), -beta, -evalValue, depth - 1);
-
-            a=std::max(a,evalValue);
-            b=a+1;
-            first=false;
-
-            if(maxValue<a) {
-                maxValue=a;
-                result=cell;
-            }
-        }
-
-        score = maxValue;
-
-        return result;
     }
-
-private:
-    i64 dfs(const Board &board, CellState color, i64 alpha, i64 beta, i32 depth) noexcept {
-        assert(alpha <= beta);
-        if(depth == 0 || board.isFinished()) {
-            return eval(board, color);
-        }
-        auto cells = board.makeReversibleCells(color);
-        if(cells.empty()) {
-            return -dfs(board, switchCellState(color), -beta, -alpha, depth);
-        }
-
-        if(depth > 6) {
-            moveOrdering(cells, board, color, 4, eval, std::greater<i64>());
-        }
-
-        i64 value;
-        i64 a=alpha;
-        i64 b=beta;
-        bool first=true;
-
-        for(const auto &cell : cells) {
-            auto nextBoard(board);
-            nextBoard.putStone(color, cell);
-
-            value = -dfs(nextBoard, switchCellState(color), -b, -a, depth - 1);
-            if(a<value && value<beta && !first) a=-dfs(nextBoard, switchCellState(color),-beta, -value, depth - 1);
-            a=std::max(a, value);
-            if(a>=beta) {
-                // cache.update(board, a, maxValue<i64>());
-                return a;
-            }
-            b=a+1;
-            first=false;
-        }
-
-        return a;
-    }
-
 };
+
+template<class Eval>
+using MidGameAI = AI<Eval, MoveOrdering<Eval>>;
+
 
 } // namespace crosswalk
 
--- test/EndGameAITest.cpp
+++ test/EndGameAITest.cpp
@@ -1,5 +1,4 @@
-#include "../src/AI.hpp"
-#include "../src/EndGame.hpp"
+#include "../src/EndGameAI.hpp"
 #include "gtest/gtest.h"
 
 class EndGameAITest : public ::testing::Test {
@@ -13,7 +12,7 @@ TEST_F(EndGameAITest, ai) {
     auto board = Board(0x7fbfbfbdbf9c0800, 0x0040404240633428);
     i64 depth = 64 - board.countStone(CellState::BLACK) - board.countStone(CellState::WHITE);
 
-    AI<EndGameEval, FFHSort> ai;
+    EndGameAI ai;
     ai.setNormalDFSDepth(6);
 
     auto cell = ai(board, CellState::BLACK, board.getEmptyCount());
@@ -22,4 +21,11 @@ TEST_F(EndGameAITest, ai) {
     EXPECT_EQ(cell.toString(), "g2");
 }
 
+TEST_F(EndGameAITest, eval) {
+    using namespace crosswalk;
+    EndGameEval eval;
+    auto board = Board(0x0076665a3a7efe80, 0xff8999a5c581017f);
 
+    EXPECT_EQ(eval(board, CellState::WHITE), 2);
+    EXPECT_EQ(eval(board, CellState::BLACK), -2);
+}
